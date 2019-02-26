759ba9438e5b32c58043255fe6dcb7906e2fa133
Arthur Gatin <agatin@umail.ucsb.edu>
2019-02-25 19:49:27 -0800

    q7 done

--- q7.cpp
+++ q7.cpp
@@ -4,6 +4,22 @@
 
 using namespace std;
 
+void numLines(string filename, char c){
+	ifstream ifs;
+	ifs.open(filename);
+	if(ifs.fail()) return;
+	string nextLine;
+	int count = 0;
+	while(true){
+		getline(ifs, nextLine);
+		if(nextLine == "") break;
+		cout << nextLine << endl;
+		if(nextLine.at(0) == c) count++;
+	}
+	cout << count << endl;
+	ifs.close();
+}
+
 int main(){
 
 	numLines("sample1.txt", 'A');
